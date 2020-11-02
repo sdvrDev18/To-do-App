918101655fcfb2baba5ff4e63d7d6da7a44969ac
Ma <chaitra.ma@accenture.com>
2020-11-02 18:29:55 +0530

    Added background Image

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,6 +1,14 @@
 import React, { Component } from "react";
 import { Button } from "@material-ui/core";
 import "./Home-style.css";
+import BackgroundImage from "../../images/background-image-1.jpeg"
+
+const backgroundStyle = {
+  width: '100%',
+  height: '800px',
+  backgroundImage: `url(${BackgroundImage})`,
+  backgroundSize: 'cover'   
+};
 
 export default class Home extends Component {
   clicked() {
@@ -8,10 +16,12 @@ export default class Home extends Component {
   }
   render() {
     return (
+      <div style={backgroundStyle}>
       <div>
         <Button className="Button-style" onClick={this.clicked}>
           boom !
         </Button>
+      </div>
       </div>
     );
   }
